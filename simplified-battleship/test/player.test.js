4e6da8b7964acc6f48caedeef36234c03d9e64a2
JLz403 <jluzz403@gmail.com>
2020-02-23 16:13:00 -0700

    mock board in testing

--- simplified-battleship/test/player.test.js
+++ simplified-battleship/test/player.test.js
@@ -1,22 +1,41 @@
+import Board from '../src/board.js'
 import Player from '../src/player.js'
 
+jest.mock('../src/board.js')
+
 var player
+var boardMockInstance
 
 beforeEach(() => {
+  Board.mockClear()
   player = new Player('1')
+  boardMockInstance = Board.mock.instances[0]
 })
 
 describe('Player', () => {
 
   test('Should init player properties', () => {
     expect(player.ID).toEqual('1')
-    expect(player.board).toBeObject()
+    expect(Board).toHaveBeenCalled()
   })
 
   describe('recieveShot', () => {
     
     test('Should place shot on their board', () => {
-      // expect(this.board.placeShot).toHaveBeenCalled
+      let shot = [0,0]
+      player.recieveShot(shot)
+      expect(boardMockInstance.placeShot).toHaveBeenCalled()
+    })
+
+  })
+
+  describe('placeShip', () => {
+    
+    test('Should place ship on their board', () => {
+      let start = [0,0]
+      let end = [0,3]
+      player.placeShip(start, end)
+      expect(boardMockInstance.placeShip).toHaveBeenCalled()
     })
 
   })
