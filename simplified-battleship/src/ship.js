64bf4118416e808e6ad63b993b54a87321d8a25a
JLz403 <jluzz403@gmail.com>
2020-02-23 18:15:33 -0700

    update ship class to include placeShip

--- simplified-battleship/src/ship.js
+++ simplified-battleship/src/ship.js
@@ -1,25 +1,51 @@
 export default class Ship {
   constructor(){
-    this.placement = new Array(3).fill({coordinate: [], hit: false})
+    this.placement = new Array(3)
     this.isSunk = false
   }
 
   place(start, end) {
-    // determine if its a valid placement
-    // let orientation = 'horizontal'
-    // // is it vertical or horizontal?
-    // if(start[0] != end[0]) {
-    //   orientation = 'vertical'
-    // }
-    // let direction = 'forwards'
-    // // is it a valid length
-    // if(orientation = 'horizontal'){
-    //   if(start[0] + 2 == end[0]){
-        
-    //   }
-    // } else {
-    //   if()
-    // }
+    // determine orientation of ship
+    let orientation = 'horizontal'
+    if(start[0] != end[0]) {
+      orientation = 'vertical'
+    }
+
+    //determine which way the ship is facing
+    //determine if ship is of valid length
+    let direction
+    if(orientation == 'horizontal'){
+      direction = 'right'
+      if (start[1] > end[1]) direction = 'left'
+      if(!(Math.abs(start[1] - end[1]) == 2)) throw "Ship is not length of 2"
+    } else if(orientation == 'vertical') {
+      direction = 'down'
+      if(start[0] > end[0]) direction = 'up'
+      if(!(Math.abs(start[0] - end[0]) == 2)) throw "Ship is not length of 2"
+    }
+
+    //create extra coordinate
+    let newCoordinate = []
+    if(orientation == 'horizontal') {
+      if(direction == 'right') {
+        newCoordinate = [start[0], start[1] + 1]
+      } else if(direction == 'left') {
+        newCoordinate = [start[0], start[1] - 1]
+      }
+    } else if (orientation == 'vertical') {
+      if(direction == 'down') {
+        newCoordinate = [start[0] + 1, start[1]]
+      } else if(direction == 'up') {
+        newCoordinate = [start[0] - 1, start[1]]
+      }
+    }
+  
+    //fill ships placement array
+    this.placement[0] = { coordinate: [start[0], start[1]], hit: false}
+    this.placement[1] = { coordinate: [newCoordinate[0], newCoordinate[1]], hit: false }
+    this.placement[2] = { coordinate: [end[0], end[1]], hit: false }
+
+    return [start, newCoordinate, end]
   }
 
   registerHit(shot) {
--- simplified-battleship/test/ship.test.js
+++ simplified-battleship/test/ship.test.js
@@ -15,13 +15,61 @@ describe('Ship', () => {
 
   describe('place', () => {
 
+    test('Should place a horizontal right facing ship', () => {
+      let start = [0,0]
+      let end = [0,2]
+      ship.place(start, end)
+      expect(ship.placement[0].coordinate[0]).toEqual(0)
+      expect(ship.placement[0].coordinate[1]).toEqual(0)
+      expect(ship.placement[1].coordinate[0]).toEqual(0)
+      expect(ship.placement[1].coordinate[1]).toEqual(1)
+      expect(ship.placement[2].coordinate[0]).toEqual(0)
+      expect(ship.placement[2].coordinate[1]).toEqual(2)
+    })
+
+    test('Should place a horizontal left facing ship', () => {
+      let start = [0,2]
+      let end = [0,0]
+      ship.place(start, end)
+      expect(ship.placement[0].coordinate[1]).toEqual(2)
+      expect(ship.placement[0].coordinate[0]).toEqual(0)
+      expect(ship.placement[1].coordinate[0]).toEqual(0)
+      expect(ship.placement[1].coordinate[1]).toEqual(1)
+      expect(ship.placement[2].coordinate[0]).toEqual(0)
+      expect(ship.placement[2].coordinate[1]).toEqual(0)
+    })
+
+    test('Should place a vertical down facing ship', () => {
+      let start = [0,0]
+      let end = [2,0]
+      ship.place(start, end)
+      expect(ship.placement[0].coordinate[0]).toEqual(0)
+      expect(ship.placement[0].coordinate[1]).toEqual(0)
+      expect(ship.placement[1].coordinate[0]).toEqual(1)
+      expect(ship.placement[1].coordinate[1]).toEqual(0)
+      expect(ship.placement[2].coordinate[0]).toEqual(2)
+      expect(ship.placement[2].coordinate[1]).toEqual(0)
+    })
+
+    test('Should place a vertical up facing ship', () => {
+      let start = [2,0]
+      let end = [0,0]
+      ship.place(start, end)
+      expect(ship.placement[0].coordinate[0]).toEqual(2)
+      expect(ship.placement[0].coordinate[1]).toEqual(0)
+      expect(ship.placement[1].coordinate[0]).toEqual(1)
+      expect(ship.placement[1].coordinate[1]).toEqual(0)
+      expect(ship.placement[2].coordinate[0]).toEqual(0)
+      expect(ship.placement[2].coordinate[1]).toEqual(0)
+    })
+
   })
 
   describe('registerHit', () => {
 
     test('Should register a hit', () => {
       let shot = [0,0]
-      ship.placement[0].coordinate = [0,0]
+      ship.placement[0] = { coordinate: [0,0], hit: false }
       ship.registerHit(shot)
       expect(ship.placement[0].hit).toBeTruthy()
     })
@@ -31,6 +79,7 @@ describe('Ship', () => {
   describe('checkSunk', () => {
 
     test('Should register ship as sunk', () => {
+      ship.placement[0] = { coordinate: [0,0], hit: false }
       ship.placement.forEach((element) => {
         element.hit = true
       })
@@ -39,6 +88,7 @@ describe('Ship', () => {
     })
 
     test('Should register ship as not sunk', () => {
+      ship.placement[0] = { coordinate: [0,0], hit: false }
       ship.checkSunk()
       expect(ship.isSunk).toBeFalsy()
     })
